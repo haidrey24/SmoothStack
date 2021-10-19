08b331125554ad65d9b842b804b89e7c30e5a235
Bruce Haidrey <brucehaidrey@Bruces-MacBook-Pro.local>
2021-10-19 14:53:10 -0700

    Finished jvb2 a2

--- JVB2/Assignment2.java
+++ JVB2/Assignment2.java
@@ -9,12 +9,53 @@ This class constructs a 2D array and finds the max number. It also show its posi
 public class Assignment2 {
 
   /*
+  * Finds the max number and its position
+  * @param arr - array to be searched
+  */
+  private void findMax(int[][] arr) {
+    // temp variables used for return
+    int max = 0;
+    int tempI = 0;
+    int tempJ = 0;
+
+    // iterate through the rows
+    for (int i = 0; i < arr.length; i++)
+    {
+      // iterate through the columns
+      for (int j = 0; j < arr[i].length; j++)
+      {
+        // if the number at this position in the array is greater than the max
+        if (arr[i][j] > max)
+        {
+          // update max
+          max = arr[i][j];
+          tempI = i;
+          tempJ = j;
+        }
+      }
+    }
+
+    // print out the max and its position
+    System.out.print("The max number in this array is: " + max);
+    System.out.print(" at position (" + tempI +"," + tempJ +")");
+
+  }
+
+  /*
   * @param args
   */
   public static void main(String[] args) {
+    // create instance of the class
+    Assignment2 a = new Assignment2();
 
-  int[][] arr; 
+    // sample arrays to test first and last spots on array
+    int[][] arr = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8, 9}};
+    int[][] arr2 = {{94, 1, 2, 76}, {4, 5}, {6, 7, 8, 9, 27}};
 
-
+    // call the functions
+    a.findMax(arr);
+    a.findMax(arr2);
   }
+
+
 }
